a6d2d6b6e0644439d3b377fea35aedbcaa7ee677
sudoBash418 <sudoBash418@gmail.com>
2022-11-10 05:15:23 -0700

    Release 0.2.1

--- cctk/constants.py
+++ cctk/constants.py
@@ -1,6 +1,6 @@
 # define global metadata
 TOOLKIT_DESCRIPTION = "CTF Challenge ToolKit - A tool to automate CTF challenge management."
-TOOLKIT_VERSION = "0.2.0"
+TOOLKIT_VERSION = "0.2.1"
 
 # define constant config file names
 REPO_CONFIG_FILENAME = "challenge-repository.toml"
