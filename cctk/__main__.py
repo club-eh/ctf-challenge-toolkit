e99c264070059e057712ad2c293755b4990eb2c5
sudoBash418 <sudoBash418@gmail.com>
2022-11-27 19:21:31 -0700

    __main__: Silence trio's sys.excepthook RuntimeWarning

--- cctk/__main__.py
+++ cctk/__main__.py
@@ -2,6 +2,13 @@
 
 from .commands import root as _cli_root
 
+# hack to silence trio's annoying RuntimeWarning regarding our custom sys.excepthook (from rich)
+import sys
+_old_hook = sys.excepthook
+sys.excepthook = sys.__excepthook__
+import trio
+sys.excepthook = _old_hook
+
 
 def entrypoint():
 	"""Run the main Click entrypoint using the Trio backend."""
